c5bffeb1b36217179c353e574d4f77289fdc1e95
Taichi Araki <t-b-araki@mercari.com>
2026-07-13 16:52:47 +0900

    feat(session): add delete session to session manager

--- internal/session/session_manager.go
+++ internal/session/session_manager.go
@@ -2,8 +2,10 @@ package session
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/TlexCypher/my-tmux-sessionizer/internal/types"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -41,6 +43,20 @@ func (sm *SessionManager) ListSessions() (sessions []*Session) {
 	return sessions
 }
 
+func (sm *SessionManager) FilterSessions(rawPaths []string) (sessions []*Session) {
+	paths := make(map[string]struct{})
+	for _, rp := range rawPaths {
+		paths[rp] = struct{}{}
+	}
+
+	for _, v := range sm.sessions {
+		if _, exists := paths[v.ProjectPath.Value()]; exists {
+			sessions = append(sessions, v)
+		}
+	}
+	return sessions
+}
+
 func (sm *SessionManager) GetSession(rawPath string) (*Session, error) {
 	projectPath := types.NewString(rawPath)
 
@@ -49,4 +65,26 @@ func (sm *SessionManager) GetSession(rawPath string) (*Session, error) {
 	}
 
 	return nil, ErrSessionNotFound
+}
+
+func (sm *SessionManager) deleteSession(rawPath string) error {
+	projectPath := types.NewString(rawPath)
+	if _, exists := sm.sessions[projectPath]; !exists {
+		return ErrSessionNotFound
+	}
+	delete(sm.sessions, projectPath)
+	return nil
+}
+
+func (sm *SessionManager) DeleteSessions(rawPaths []string) error {
+	var eg errgroup.Group
+	for _, path := range rawPaths {
+		eg.Go(func() error {
+			if err := sm.deleteSession(path); err != nil {
+				return err
+			}
+			return nil
+		})
+	}
+	return nil
 }
